9b9155404a8aefebabea6cb57f342dc3cbc843c7
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-12-23 21:14:07 +0100

    the content of the cards now changed when the toggle btn is used

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 toggle_btn = document.querySelector('[data-toggle-btn]')
 
+price_basic = document.querySelector('[data-basic-price]')
+price_pro = document.querySelector('[data-pro-price]')
+price_master = document.querySelector('[data-master-price]')
+
 toggle_btn.addEventListener('click', function(e) {
     toggle_btn.classList.toggle('toggle-btn--off')
+
+    if(toggle_btn.classList.contains('toggle-btn--off')) { 
+        // annualy
+       
+        price_basic.innerHTML = "199.99";
+        price_pro.innerHTML = "249.99";
+        price_master.innerHTML = "399.99";
+    }
+    else {
+        // monthly
+       
+        price_basic.innerHTML = "19.99";
+        price_pro.innerHTML = "24.99";
+        price_master.innerHTML = "39.99";
+
+    }
 })
